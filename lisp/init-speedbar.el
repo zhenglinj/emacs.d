dfddb9d364c192a1eff122ebb6014774d2d6516e
zhenglinj <zhenglinj@hotmail.com>
2015-01-27 23:21:18 +0800

    Config speedbar default setting

--- lisp/init-speedbar.el
+++ lisp/init-speedbar.el
@@ -2,10 +2,10 @@
 (require 'speedbar)
 (setq speedbar-show-unknown-files t)
 (setq speedbar-verbosity-level 0)
-(setq speedbar-frame-parameters (quote
-                                 ((minibuffer)
-                                  (width . 35)
-                                  )))
+;; (setq speedbar-frame-parameters (quote
+;;                                  ((minibuffer)
+;;                                   (width . 35)
+;;                                   )))
 ;; (setq speedbar-use-images nil)
 
 (global-set-key [(meta f11)] 'speedbar-get-focus)
