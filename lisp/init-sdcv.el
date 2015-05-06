65506f478ed76d8c6fc861ff99b3e14ee9f526b0
zhenglinj <zhenglinj@hotmail.com>
2015-05-07 03:43:02 +0800

    Abandon a dictionary

--- lisp/init-sdcv.el
+++ lisp/init-sdcv.el
@@ -8,7 +8,7 @@
         "朗道英汉字典5.0"
         ;; "懒虫简明英汉词典"
         "牛津现代英汉双解词典"
-        "21世纪英汉汉英双向词典"
+        ;; "21世纪英汉汉英双向词典"
         "WordNet"))
 (autoload 'sdcv-search-pointer+ "sdcv" "show word explanation in buffer" t)
 (autoload 'sdcv-search-input "sdcv" "show word explanation in tooltip" t)
