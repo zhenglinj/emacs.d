ace0394ec1d57a04f518b8d2c98f0dfcb13ba5dd
zhenglinj <zhenglinj@hotmail.com>
2015-05-07 04:53:31 +0800

    Use "pandoc" instead of "markdown"

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,4 +3,9 @@
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
 
+;; Latex support: --mathjax
+;; Syntax highlight: --highlight-style pygments|kate|monochrome|espresso|zenburn|haddock|tango
+;; Use CSS: -c /home/zhenglj/.emacs.d/site-lisp/emacs-mkdown/mkdown.css
+(setq markdown-command "pandoc -f markdown -t html -s --mathjax --highlight-style espresso")
+
 (provide 'init-markdown)
