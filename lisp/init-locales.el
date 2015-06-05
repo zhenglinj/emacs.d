d80782df7da18164d77f351d51f81c1cda446b42
zhenglinj <zhenglinj@hotmail.com>
2015-06-05 22:10:44 +0800

    Set set-selection-coding-system on Windows/Cygwin

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -15,7 +15,10 @@
   (setq locale-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
-  (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
+  (set-selection-coding-system (if (or (eq system-type 'windows-nt)
+                                       (eq system-type 'cygwin))
+                                   'utf-16-le ;default windows unicode or 'gb2312
+                                 'utf-8))
   (prefer-coding-system 'utf-8))
 
 (provide 'init-locales)
