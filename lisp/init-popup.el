3ee604925d06a0c5a1094afc58b2e3caf8ddd1f4
zhenglinj <zhenglinj@hotmail.com>
2015-11-11 11:09:40 +0800

    Fix conflict between popup-pos-tip and powerline

--- lisp/init-popup.el
+++ lisp/init-popup.el
@@ -1,10 +1,14 @@
 (require 'popup)
-(require 'popup-pos-tip)
 
-(defadvice popup-tip
-  (around popup-pos-tip-wrapper (string &rest args) activate)
-  (if (eq window-system 'x)
-      (apply 'popup-pos-tip string args)
-    ad-do-it))
+(if (not (or (eq system-type 'ms-dos)
+             (eq system-type 'windows-nt)
+             (eq system-type 'cygwin)))
+    (require 'popup-pos-tip)
+  (defadvice popup-tip
+      (around popup-pos-tip-wrapper (string &rest args) activate)
+    (if (eq window-system 'x)
+        (apply 'popup-pos-tip string args)
+      ad-do-it))
+  )
 
 (provide 'init-popup)
