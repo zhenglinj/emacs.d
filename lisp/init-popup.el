0d43ee9148f8ba4c10d5480f0b2d25e4958eb5cb
zhenglinj <zhenglinj@hotmail.com>
2015-11-23 14:25:59 +0800

    Init popup from elpa

--- lisp/init-popup.el
+++ lisp/init-popup.el
@@ -1,14 +1,15 @@
+(require-package 'popup)
 (require 'popup)
 
+(require-package 'popwin)
+(require 'popwin)
+(popwin-mode 1)
+
+(require-package 'pos-tip)
 (if (not (or (eq system-type 'ms-dos)
              (eq system-type 'windows-nt)
              (eq system-type 'cygwin)))
-    (require 'popup-pos-tip)
-  (defadvice popup-tip
-      (around popup-pos-tip-wrapper (string &rest args) activate)
-    (if (eq window-system 'x)
-        (apply 'popup-pos-tip string args)
-      ad-do-it))
+    (require 'pos-tip)
   )
 
 (provide 'init-popup)
