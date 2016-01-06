cf85910554f16428da605aaaad107344123a0c99
zhenglinj <zhenglinj@hotmail.com>
2016-01-06 21:50:12 +0800

    Update markdown table (like org table)

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -9,4 +9,18 @@
 ;; Use CSS: -c /home/zhenglj/.emacs.d/site-lisp/emacs-mkdown/mkdown.css
 (setq markdown-command "pandoc -f markdown -t html -s --mathjax --highlight-style espresso")
 
+;; http://stackoverflow.com/questions/14275122/editing-markdown-pipe-tables-in-emacs
+(require 'org-table)
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))
+    ))
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)
+            (define-key markdown-mode-map (kbd "C-c RET") 'org-table-hline-and-move)
+            ))
+
 (provide 'init-markdown)
