aae87748f8953246059f58b79b05e1069b04cc8c
zhenglinj <zhenglinj@hotmail.com>
2016-03-30 19:59:47 +0800

    Update popup configuration for OSX

--- lisp/init-popup.el
+++ lisp/init-popup.el
@@ -2,14 +2,14 @@
 (require 'popup)
 
 (require-package 'popwin)
-(require 'popwin)
-(popwin-mode 1)
-
 (require-package 'pos-tip)
 (if (not (or (eq system-type 'ms-dos)
              (eq system-type 'windows-nt)
              (eq system-type 'cygwin)))
-    (require 'pos-tip)
+    (progn
+      (require 'popwin)
+      (require 'pos-tip)
+      (popwin-mode 1))
   )
 
 (provide 'init-popup)
