8233df851a0dd195d797f5349e4af03279cd0de9
zhenglinj <zhenglinj@hotmail.com>
2016-07-14 22:42:27 +0800

    Update realgud key binding

--- lisp/init-realgud.el
+++ lisp/init-realgud.el
@@ -1,4 +1,6 @@
 (require-package 'realgud)
 (require 'realgud)
 
+(define-key realgud:shortkey-mode-map [mouse-3] 'realgud:tooltip-eval) ; right button / double finger touchpad
+
 (provide 'init-realgud)
