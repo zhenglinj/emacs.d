f5eb177a72a8af19dabf0427b5962f623cf9a2e6
zhenglinj <zhenglinj@hotmail.com>
2017-06-11 18:11:37 +0800

    Update mode for reactjs

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -100,4 +100,82 @@
               (lambda () (inferior-js-keys-mode -1)))))
 
 
+;; ---------------------------------------------------------------------------
+;; Auto-complete for JavaScript
+;; ---------------------------------------------------------------------------
+;; ac-js2
+(when (maybe-require-package 'ac-js2-mode)
+  (add-hook 'js2-mode-hook (lambda () (ac-js2-mode))))
+
+;; tern
+(maybe-require-package 'tern)
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+(if (eq system-type 'windows-nt)
+    (setq tern-command '("node" "<TERN LOCATION>\\bin\\tern")))
+
+(maybe-require-package 'tern-auto-complete)
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+;; web-beautify
+(maybe-require-package 'web-beautify)
+
+
+;; ---------------------------------------------------------------------------
+;; For React.js
+;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+;; ---------------------------------------------------------------------------
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(json-jsonlist)))
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js" root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-jsx-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  ;; js2-mode
+  (setq js2-basic-offset 2)
+  ;; web-mode
+  (setq css-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-jsx-mode-hook)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+
 (provide 'init-javascript)
